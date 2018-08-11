47c2266afd3696bece4bfabc88071b2dd6852890
geedha <32114899+geedha@users.noreply.github.com>
2018-08-11 20:29:56 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,21 +2,21 @@ import java.io.*;
 class Billcontents
 {
 int consumer_no;
-String name;
+String consumer_name;
 int previous_reading;
 int current_reading;
 int type_of_connection;
-double sum;
-
+double rupees;
+double pri;
 Billcontents()
 {
 consumer_no=0;
-name="noname";
+consumer_name="noname";
 previous_reading=0;
 current_reading=0;
-String name; 
-sum=0.00;
+
 }
+
 void getDetails() throws IOException
 {
 String s_consumer_no;
@@ -25,102 +25,110 @@ String s_current_reading;
 String s_type_of_connection;
 InputStreamReader insr = new InputStreamReader(System.in);
 BufferedReader bfr = new BufferedReader(insr);
-System.out.println("Enter the name");
-name = bfr.readLine();
-
-System.out.println("Enter '1' if domestic connection");
-System.out.println("Enter '2' if commercial connection");
-s_type_of_connection = bfr.readLine();
-type_of_connection=Integer.parseInt(s_type_of_connection);
-System.out.println("Enter the consumer number");
+System.out.println("ENTER '1' IF DOMESTIC CONNECTION");
+System.out.println("ENTER '2' IF COMMERCIAL CONNECTION");
+s_type_of_connection=bfr.readLine();
+type_of_connection= Integer.parseInt(s_type_of_connection);
+System.out.println("ENTER THE CONSUMER NUMBER");
 s_consumer_no=bfr.readLine();
-consumer_no=Integer.parseInt(s_consumer_no);
-System.out.println("Enter the previous month unit number");
+consumer_no = Integer.parseInt(s_consumer_no);
+System.out.println("ENTER THE PREVIOUS MONTH UNIT");
 s_previous_reading=bfr.readLine();
-previous_reading=Integer.parseInt(s_previous_reading);
-System.out.println("Enter the current  month unit number");
+previous_reading= Integer.parseInt(s_previous_reading);
+System.out.println("ENTER THE CURRENT MONTH UNIT");
 s_current_reading=bfr.readLine();
-current_reading=Integer.parseInt(s_current_reading);
+current_reading= Integer.parseInt(s_current_reading);
+}
+double slabOne()
+{
+int correct_reading = current_reading-previous_reading; 
+if(correct_reading<=100)
+  {
+      rupees = correct_reading*1;
+      return rupees;
+ }
+else 
+{
+ rupees = 100*1;
+return rupees;
+}
+
 
 }
-void compute_bill()
+ double secondSlab()
 {
-int i;
-int ori_unit;
-
-ori_unit=current_reading-previous_reading;
-switch(type_of_connection)
+int correct_reading = current_reading-previous_reading; 
+    pri=correct_reading-100;
+if(pri>100)
 {
-case 1:
-for(i=1;i <= ori_unit;i++)
-{
-if(i<=100)
-{ 
- sum = sum+1.00;
-
+    rupees= (100*2.50);
+     return rupees;
 }
-else if(i>100 && i<=200)
+else
 {
-
-sum=sum+02.50;
-
+rupees=pri*2.50;
+return rupees;
 }
-else if(i>200 && i<=500)
+ }
+double thirdSlab()
 {
-	sum=sum+04.00;
-  
-}
-else{
-	sum=sum+06.00;
-   
-}
-}
-break;
-case 2:
-for(i=0;i<=ori_unit;i++)
+int correct_reading = current_reading-previous_reading; 
+ pri=correct_reading-200;
+if(pri>100)
 {
-if(i<=100)
+    rupees = (100*4);
+     return rupees;
+}
+else
 {
- sum = sum+2;
+rupees=pri*4;
+return rupees;
 
 }
-else if(i>100 && i<=200)
+}
+double  fourthSlab()
 {
+int correct_reading = current_reading-previous_reading; 
+ pri=correct_reading-300;
+if(pri>100)
+{
+    rupees = (100*6);
+    
+     return rupees;
+}
+else
+{
+  rupees=pri*6;
+   return rupees;
+}
 
-sum+=4.50;
+ }
 
-}
-else if(ori_unit>200 && ori_unit<=500)
-{
-	
-	sum+=6;
-  
-}
-else{
-	
-	sum+=7;
-   
-}
-}
-break;
-default:
-break;
-}
-}
-void displaybill()
-{
-	System.out.println("amount is"+sum);
-	System.out.println("name is"+name);
-}
-}
+
+  }
 class Main
 {
-	public static void main(String[] args)throws IOException
-	{
-		Billcontents m = new Billcontents();
-		m.getDetails();
-		m.compute_bill();
-		m.displaybill();
-	}
+public static void main(String[] args)throws IOException
+{
+
+Billcontents m=new Billcontents();
+m.getDetails();
+double ans;
+switch(m.type_of_connection)
+{
+case 1: 
+  ans=m.slabOne()+m.secondSlab()+m.thirdSlab()+m.fourthSlab();
+  System.out.println("answer is"+ans);
+ break;
+  default:
+   break;
+   }
 }
+}
+
+
+   
+    
+
+
 
