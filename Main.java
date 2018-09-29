0b201efe96bdeb15ae8eb5911b87f89d00089ed3
geedha <32114899+geedha@users.noreply.github.com>
2018-09-29 15:02:44 +0530

    fileclass

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 import java.io.*;
+import java.util.Scanner;
 public class Main {
+	String filename;
 
-	
 	public static void main(String[] args) {
-		
-		Programmer p = new Programmer(50000);
-		p.generatepayslip(50000);
-		Assistantprof a = new Assistantprof(20000);
-		a.generatepayslip(20000);
+		String filename = new Scanner(System.in).nextLine();
+		File fb = new File(filename);
+		if(fb.exists())
+			System.out.println("file exists");
+		if(fb.canRead())
+			System.out.println("File is readable");
+		if(fb.canWrite())
+			System.out.println("Writable");
+		System.out.println(fb.getName());
+		System.out.println(fb.length());
 	}
 
 }
